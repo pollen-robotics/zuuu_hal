dc30b7215765a2d91503b2c1c3f58a1bd55a60f1
RemiFabre <remifabre1800@gmail.com>
2022-01-31 21:42:22 +0100

    Follow me lidar integration wip

--- zuuu_follow_me/follow_me.py
+++ zuuu_follow_me/follow_me.py
@@ -2,10 +2,13 @@ import rclpy
 from rclpy.node import Node
 import pygame
 import time
+import math
 import numpy as np
 import traceback
 import sys
 from pyvesc.VESC import MultiVESC
+from sensor_msgs.msg import LaserScan
+import cv2
 
 
 # Button  0 = X
@@ -51,6 +54,13 @@ class FollowMe(Node):
         super().__init__('follow_me')
         self.get_logger().info("Starting follow me behavior!")
 
+        self.laser_scan = None
+        self.create_subscription(
+            LaserScan,
+            '/scan',
+            self.scan_callback,
+            1)
+
         # we could tick pygame with a callback:
         # self.create_timer(0.2, self.timer_callback)
         pygame.init()
@@ -66,6 +76,9 @@ class FollowMe(Node):
         self.omnibase = MobileBase()
         self.lin_speed_ratio = 0.2
         self.rot_speed_ratio = 0.1
+        self.loop_freq = 100
+        self.clock = pygame.time.Clock()
+        self.t0 = time.time()
 
     def emergency_shutdown(self):
         self.get_logger().warn("Emergency shutdown!")
@@ -73,6 +86,105 @@ class FollowMe(Node):
         self.omnibase.left_wheel.set_duty_cycle(0)
         self.omnibase.right_wheel.set_duty_cycle(0)
         sys.exit(1)
+
+    def scan_callback(self, msg):
+        self.laser_scan = msg
+
+    def angle_diff(a, b):
+        # Returns the smallest distance between 2 angles
+        d = a - b
+        d = ((d + math.pi) % (2 * math.pi)) - math.pi
+        return d
+
+    def get_barycenter_offset(self, range_min, range_max, detection_angle, verbose=True):
+        """Listen to the /scan LaserScan topic and outputs dlin_percent and dang_percent which represent how far away from the center of the chosen shape the barycenter of points is.
+        """
+        # frame_id: "base_scan"
+        # angle_min: 0.0
+        # angle_max: 6.2657318115234375
+        # angle_increment: 0.01745329238474369
+        # time_increment: 2.9880000511184335e-05
+        # scan_time: 0.0
+        # range_min: 0.11999999731779099
+        # range_max: 3.5
+        if self.laser_scan is None:
+            return 0, 0
+        absolute_range_max = 3.5
+        absolute_angle_max = 6.2657318115234375
+        half_angle = detection_angle/2
+        # rospy.loginfo("Waiting for laser scan")
+        # laser_scan = rospy.wait_for_message("/tb3/scan", LaserScan, timeout=None)
+        # rospy.loginfo("laser scan received!")
+
+        angle_increment = self.laser_scan.angle_increment
+        pixel_per_meter = 250
+        image_size = int(absolute_range_max * pixel_per_meter)
+        height = image_size
+        width = image_size
+        self.get_logger().info("Image will be {}x{}".format(width, height))
+        image = np.zeros((height, width, 3), np.uint8)
+        index = -1
+        center_x = width / 2
+        center_y = 0.5*(range_max + range_min) * pixel_per_meter
+        sum_x = 0
+        sum_y = 0
+
+        nb_points = 0
+        if verbose:
+            for i in range(width):
+                for j in range(height):
+                    angle = math.atan2(i-(width/2), j)
+                    dist = math.sqrt(((width/2)-i)**2 + j**2)/pixel_per_meter
+                    if dist >= range_min and dist <= range_max and abs(self.angle_diff(angle, 0)) < half_angle:
+                        image[j, i] = (50, 50, 100)  # y, x as always
+
+        # the 0, 0 point of the laserscan will be drawn on the point x = width/2 and y = 0 (so the top of the image)
+        for r in self.laser_scan.ranges:
+            index += 1
+            d = 0
+            try:
+                d = float(r)
+                if np.isnan(d):
+                    continue
+            except:
+                # inf?
+                continue
+            if d < range_min or d >= range_max:
+                continue
+            angle = self.laser_scan.angle_min + index * angle_increment
+            if abs(self.angle_diff(angle, 0)) > half_angle:
+                continue
+            x = int(round(width / 2 + d * pixel_per_meter * math.sin(angle)))
+            y = int(round(d * pixel_per_meter * math.cos(angle)))
+            sum_x += x
+            sum_y += y
+            nb_points += 1
+            if x >= 0 and x < width and y >= 0 and y < height:
+                image[y, x] = (255, 255, 255)  # y, x as always
+            # print("Adding point {}".format(round(d*math.cos(angle)), round(width/2 + d*math.sin(angle))))
+        if nb_points > 0:
+            sum_x = sum_x/nb_points
+            sum_y = sum_y/nb_points
+            image[int(sum_y), int(sum_x)] = (255, 0, 0)  # y, x as always
+            # Careful, x in image frame is -y lidar frame, and y in image frame is x in lidar frame
+            avg_angle = math.atan2(sum_x - width/2, sum_y)
+            dist = math.sqrt((sum_x-center_x)**2 + (sum_y-center_y)**2)
+            if center_y < sum_y:
+                dist *= -1
+            dlin_percent = max(-1, min(1, 2*dist /
+                               (pixel_per_meter*(range_max - range_min))))
+            dang_percent = max(-1, min(1, avg_angle/half_angle))
+            self.get_logger().info("avg_x_pixels={}, avg_y_pixels){}, avg_angle={}, dist_pixels={}, dlin_percent={}, dang_percent={}, center_y={}".format(
+                sum_x, sum_y, avg_angle, dist, dlin_percent, dang_percent, center_y))
+        else:
+            dlin_percent = 0
+            dang_percent = 0
+
+        if verbose:
+            cv2.imshow("image", image)
+            # cv2.imwrite("raw" + str(datetime.utcnow())+".png", image)
+            cv2.waitKey(1)
+        return dlin_percent, dang_percent
 
     def tick_controller(self):
         for event in pygame.event.get():
@@ -138,6 +250,17 @@ class FollowMe(Node):
         x = self.j.get_axis(0) * cycle_max_t
         rot = -self.j.get_axis(3) * cycle_max_r
 
+        return self.ik_vel(x, y, rot)
+
+    def ik_vel(self, x, y, rot):
+        """Takes 2 linear speeds and 1 rot speed and outputs the PWM to apply to each of the 3 motors in an omni setup 
+
+        Args:
+            x (float): x speed (between 0 and 1)
+            y (float): y speed (between 0 and 1)
+            rot (float): rotational speed (between 0 and 1)
+        """
+
         cycle_back = x + rot
         cycle_right = (x*np.cos(120*np.pi/180)) + \
             (y*np.sin(120*np.pi/180)) + rot
@@ -150,20 +273,35 @@ class FollowMe(Node):
         self.get_logger().info(
             "Started. Press O for emergency stop. Press triangle to change modes.")
         while True:
+            t = time.time()
+            dt = t - self.t0
+            if dt == 0:
+                f = 0
+            else:
+                f = 1/dt
+            self.get_logger().info("Potential freq: {:.0f}Hz".format(f))
+            # Caping the actual freq
+            self.clock.tick(self.loop_freq)
+            self.t0 = time.time()
+
             self.tick_controller()
             if self.mode == CONTROLLER:
                 duty_cycles = self.cycle_from_joystick()
-                self.omnibase.back_wheel.set_duty_cycle(
-                    duty_cycles[0])
-                self.omnibase.left_wheel.set_duty_cycle(
-                    duty_cycles[2])
-                self.omnibase.right_wheel.set_duty_cycle(
-                    duty_cycles[1])
+
             elif self.mode == FOLLOW_ME:
-                pass
+                dlin_percent, dang_percent = self.get_barycenter_offset(
+                    0.3, 0.7, math.pi/5)
+                duty_cycles = self.ik_vel(dlin_percent, 0, dang_percent)
             else:
                 self.get_logger().warn("Can't happen ffs")
                 self.emergency_shutdown()
+            # Actually sending the commands
+            # self.omnibase.back_wheel.set_duty_cycle(
+            #     duty_cycles[0])
+            # self.omnibase.left_wheel.set_duty_cycle(
+            #     duty_cycles[2])
+            # self.omnibase.right_wheel.set_duty_cycle(
+            #     duty_cycles[1])
 
 
 def main(args=None):
